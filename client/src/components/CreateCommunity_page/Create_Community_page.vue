4c00fca1e4b7225aec83d761bec7ff6cc635b5ca
rithchanvirakboth <74288085+rithchanvirakboth@users.noreply.github.com>
2021-07-07 19:34:05 +0700

    Update Create_Community_page.vue

--- client/src/components/CreateCommunity_page/Create_Community_page.vue
+++ client/src/components/CreateCommunity_page/Create_Community_page.vue
@@ -1,52 +1,51 @@
 <template>
-    <div class="row justify-content-center">
+    <div class="row justify-content-center"> 
         <div class="col-md-6">
-            <h3 class="text-center">Sart your community</h3>
-            <form @submit.prevent="handleSubmitForm">
-                <div class="form-group">
-                    <label>Name</label>
-                    <input type="text" class="form-control" v-model="community.name" required>
-                </div>
-
-                <div class="form-group">
-                    <label>Bio</label>
-                    <input type="text" class="form-control" v-model="community.bio">
-                </div>
-
-                <div class="form-group">
-                    <button class="btn btn-danger btn-block">Create</button>
-                </div>
-            </form>
+            <h2 class="text-center"> Create your community page </h2>
+                <div class="row justify-content-center">
+                <form @submit.prevent="handleSubmitform">
+                    <div class="form-group">
+                        <label>Community name </label>
+                        <input type="text" class="form-control" v-model="community.name" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Major </label>
+                        <input type="text" class="form-control" v-model="community.major">
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-outline-success">Create</button>
+                    </div>
+                </form>   
+            </div>
         </div>
     </div>
 </template>
-
 <script>
-import axios from "axios";
-
-    export default {
-        data() {
-            return {
-                community: {
-                    name: '',
-                    bio: ''
+export default {
+    data() {
+        return {
+            community: {
+                name: '',
+                major: ''
                 }
             }
         },
         methods: {
-            handleSubmitForm() { 
-                let apiURL = 'http://localhost:4000/api/create-community';
-                
-                axios.post(apiURL, this.community).then(() => {
-                    this.$router.push('/view')
-                    this.community = {
-                    name: '',
-                    bio: ''
-                    }
-                }).catch(error => {
-                    console.log(error)
-                });
-            }
+            handleSubmitForm() { }
         }
     }
+
 </script>
+<style>
+@import 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
+.form-control {
+    width: 100%;
+    text-align:center;
+}
+h2 {
+    color: white;
+}
+label {
+    color: white;
+}
+</style>
